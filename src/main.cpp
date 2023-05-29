f302c2bbd099a654bac14c551b069a295f673c3c
MiroslavBurda <35538660+MiroslavBurda@users.noreply.github.com>
2023-05-29 16:02:59 +0200

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,59 @@
+#include <Arduino.h>
+
+#include "roboruka.h"
+
+#define GRIDUI_LAYOUT_DEFINITION
+#include "layout.h"
+
+void setup() {
+    rkConfig cfg;
+    cfg.owner = "VasekBurda"; // Ujistěte se, že v aplikace RBcontrol máte nastavené stejné
+    cfg.name = "Roboruka2";
+
+    // Ve výchozím stavu lze WiFi na robotovi nastavit pomocí Android aplikace
+    // RBControl (verze 1.0 nebo novější) přes Bluetooth.
+    // Robot si toto nastavení pamatuje, a znovu ho použije při dalším zapnutí.
+
+    // Můžete WiFi i přímo nastavit zde v kódu, v takovém případě se nastavení
+    // nedá z aplikace změnit.
+
+    // Zde v kódu můžete BUĎTO připojit robota na WiFi...
+    //cfg.wifi_name = "RukoKraj";
+    //cfg.wifi_password = "PlnoRukou";
+
+    // A NEBO vytvořit vlastní WiFi (odkomentovat další dva řádky)
+    cfg.wifi_default_ap = true;
+    cfg.wifi_ap_password = "12345678";
+
+    cfg.motor_polarity_switch_left = true;
+    cfg.motor_polarity_switch_right = true;
+    cfg.motor_enable_failsafe = true;
+    cfg.rbcontroller_app_enable = true;
+    cfg.battery_coefficient = 0.94;
+    rkSetup(cfg);
+
+    auto builder = Layout.begin();
+    builder.Arm1
+        .onPositionChanged([&](Arm& arm) {
+            rkArmMoveTo(arm.x(), arm.y());
+        })
+        .onGrab([&](Arm&) {
+            rkArmSetGrabbing(!rkArmIsGrabbing());
+        });
+
+    builder.Joystick1
+        .onPositionChanged([&](Joystick& joy) {
+            rkMotorsJoystick(joy.x(), joy.y());
+        });
+
+    builder.commit();
+
+    fmt::print("{}'s roboruka '{}' started!\n", cfg.owner, cfg.name);
+
+    for (int i = 0; true; ++i) {
+        // Send text to the android application
+        rkControllerSendLog(fmt::format("Tick #{}, battery at {}%, {}mv\n",
+            i, rkBatteryPercent(), rkBatteryVoltageMv()));
+        delay(1000);
+    }
+}
